66d0eea90e50dbfbb61afd7bf55a8b034b1d3c90
waqas <wmunir232@gmail.com>
2024-03-24 17:04:56 +0500

    sorting & reversing lists

---
+++ Python Basics/03-introducting-lists/organizing_list.py
@@ -0,0 +1,13 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+cars.sort(reverse=True) # modifies original list
+sorted_cars = sorted(cars) # returns the sorted list
+
+print(cars)
+print(sorted_cars)
+
+
+cars.reverse() # reverses the original array
+print(cars)
+
+print(len(cars)) # give the length of the list
