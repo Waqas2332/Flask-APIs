6aed9e6473a3fa33c0775b6f5d0743edb3f4b1a1
waqas <wmunir232@gmail.com>
2024-03-25 14:39:09 +0500

    done with list slicing

---
+++ Python Basics/04-working-with-lists/list_slicing.py
@@ -0,0 +1,10 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+print(players[2:]) # if you dont specify the last limit, it will go till the end
+print(players[:4]) # if you dont specify the first limit, it will start from the start 
+
+# Looping through the slice
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
