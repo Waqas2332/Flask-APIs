54960a564f33648e6b962fc29210ea339982de3e
waqas <wmunir232@gmail.com>
2024-03-27 20:28:06 +0500

    done with dictionaries

---
+++ Python Basics/06-dictionaries/dict.py
@@ -0,0 +1,10 @@
+# dictionary is the collection of key value pairs
+
+myself = {
+    "name":"Waqas",
+    "age":23
+}
+
+del myself["age"] # for deleting key value pair from dict
+
+print(myself)
---
+++ Python Basics/06-dictionaries/loop_dict.py
@@ -0,0 +1,12 @@
+user = {
+    "firstName":"Waqas",
+    "lastName":"Munir",
+    "age":12
+}
+
+for key,values in user.items():
+    print(key)
+    
+# looping through keys
+for item in user.keys():
+    print(item)
