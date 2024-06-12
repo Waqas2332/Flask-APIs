563b68c934ed4b6742d9d83cf89de44139e019eb
waqas <wmunir232@gmail.com>
2024-06-12 19:39:57 +0500

    Created First API endpoint

--- app.py
+++ app.py
@@ -2,3 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Data store
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            },
+            {
+                "name": "Table",
+                "price": 19.99
+            }
+        ]
+    }
+]
+
+# API Route
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
